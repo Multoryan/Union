bff7c9f2ab23e1728374273e96c684706c64ecc5
RandomIt <random.it@yandex.ru>
2017-10-21 22:13:38 +0700

    Возможность смена языка в дальнейшем
    - Тексты вынесены в отдельные файлы JSON, для возможности смены языка
    
    - Исправлена ошибка во Vue

--- index.js
+++ index.js
@@ -5,21 +5,49 @@ const app = express()
 const MongoClient = require("mongodb").MongoClient;
 // Шаблонизатор Vue
 const expressVue = require('express-vue');
-
+// Общедоступные ресурсы
+app.use(express.static(__dirname + '/public'));
+// Настройки Vue.js
 const vueOptions = {
     rootPath: './views',
     layout: {
-        start: '<div id="app">',
-        end: '</div>'
+        html: {
+            start: '<!DOCTYPE html><html>',
+            end: '</html>'
+        },
+        body: {
+            start: '<body>',
+            end: '</body>'
+        },
+        template: {
+            start: '<div id="app">',
+            end: '</div>'
+        }
+    },
+    vue: {
+        head: {
+            meta: [
+                { script: "./js/vue.min.js" }
+            ]
+        }
     }
 };
+// Загрузка файла с языком
+// TODO: Добавить автоопределение на основе req.acceptsLanguages()
+// TODO: Возможность изменять язык
+const lang = require('./lang/ru');
 
+// Использование Vue.js
 const expressVueMiddleware = expressVue.init(vueOptions);
 app.use(expressVueMiddleware);
 
 
 app.get('/', function (req, res) {
   res.send('Express работает')
+})
+
+app.get('/registration', function (req, res) {
+  res.renderVue('registration',lang.registration);
 })
 
 app.get('/vue', function(req, res, next) {
---
+++ views/registration.vue
@@ -0,0 +1,16 @@
+<template>
+  <form class="registration">
+    <input type="email" placeholder="E-mail" />
+    <input type="submit" :value='buttonValue'>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
